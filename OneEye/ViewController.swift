55054684846725aaa14a7b04b69f4a6f08c6d60b
Georges Nomicos <georgesnomicos@gmail.com>
2020-05-31 15:37:38 +0200

    Initial Commit

---
+++ OneEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneEye
+//
+//  Created by Georges on 31/05/2020.
+//  Copyright © 2020 Nomicos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
